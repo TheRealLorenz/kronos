661a89fe596be1a781f5e3b230c2f64a4fa9b19f
TheRealLorenz <lollobellina@gmail.com>
2026-03-15 12:37:49 +0100

    [cleanup] remove ngrok plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,8 @@
 import devtoolsJson from 'vite-plugin-devtools-json';
-import { ngrok } from "vite-plugin-ngrok";
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig, loadEnv } from 'vite';
-
-const { NGROK_AUTH_TOKEN } = loadEnv('', process.cwd(), 'NGROK')
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    sveltekit(),
-    devtoolsJson(),
-    ngrok({
-      authtoken: NGROK_AUTH_TOKEN,
-    }),
-  ]
+  plugins: [tailwindcss(), sveltekit(), devtoolsJson()]
 });
